6e77b343d09ca09b6be3ec039043ddecf2a062cb
Ariadna <70442401+ariadnapm@users.noreply.github.com>
2024-03-25 12:35:52 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,10 +15,10 @@ st.markdown(
     **👈 Select a tool from the sidebar:
     of what Streamlit can do!
     ### Are you working with chromatograms?
-    - Do you want to view plots? Go [here](https://github.com/ariadnapm/streamlit/blob/main/pages/3_🌍_Chromatogram_Plotter.py)
+    - Do you want to view plots? Go [here](https://github.com/ariadnapm/streamlit/blob/main/3_🌍_Chromatogram_Plotter.py)
     ### Do you need to work with OPUS files?
-    - Convert them into easy to understand Excel files, using this [tool](https://github.com/ariadnapm/streamlit/blob/main/pages/2_📊_OPUS_converter.py)
+    - Convert them into easy to understand Excel files, using this [tool](https://github.com/ariadnapm/streamlit/blob/main/2_📊_OPUS_converter.py)
     ### Do you have funny formats as files?
-    - Do you just want to convert text and other formatted files into ".xlsx" files? Click [here](https://github.com/ariadnapm/streamlit/blob/main/pages/1_📈_Clean_All_Files.py)
+    - Do you just want to convert text and other formatted files into ".xlsx" files? Click [here](https://github.com/ariadnapm/streamlit/blob/main/1_📈_Clean_All_Files.py)
 """
 )
