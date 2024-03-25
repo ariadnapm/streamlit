3ab1f8a346ab560eb5cad184830eb0a1d30ef462
Ariadna <70442401+ariadnapm@users.noreply.github.com>
2024-03-25 12:30:20 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -17,7 +17,7 @@ st.markdown(
     ### Are you working with chromatograms?
     - Do you want to view plots? Go [here](https://github.com/ariadnapm/streamlit/blob/main/pages/3_🌍_Chromatogram_Plotter.py)
     ### Do you need to work with OPUS files?
-    - Convert them into easy to understand Excel files, using this [tool](https://github.com/ariadnapm/streamlit/blob/main/pages/OPUS_converter.py)
+    - Convert them into easy to understand Excel files, using this [tool](https://github.com/ariadnapm/streamlit/blob/main/pages/2_📊_OPUS_converter.py)
     ### Do you have funny formats as files?
     - Do you just want to convert text and other formatted files into ".xlsx" files? Click [here](https://github.com/ariadnapm/streamlit/blob/main/pages/clean_all_files.py)
 """
