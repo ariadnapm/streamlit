e937051ee2397401c51ec78cfd24722e0e6860e9
Ariadna <70442401+ariadnapm@users.noreply.github.com>
2024-03-26 15:37:51 +0100

    Update 3_🌍_Chromatogram_Plotter.py

--- pages/3_🌍_Chromatogram_Plotter.py
+++ pages/3_🌍_Chromatogram_Plotter.py
@@ -105,7 +105,7 @@ with col2:
         with col1:
             minimumx = st.slider("Select minimum value for the x-axis of the plot", min_x, max_x, min_default)
             maximumx = st.slider("Select maximum value for the x-axis of the plot", min_x, max_x, max_default)
-            option = st.selectbox('Legend view', (df.columns[0], df.columns[1], df.columns[2], df.columns[3], df.columns[4], df.columns[5], df.columns[6], df.columns[7], df.columns[8], df.columns[9]))
+            option = st.selectbox('Legend view', (df.columns[0], df.columns[1], df.columns[2], df.columns[3], df.columns[4], df.columns[5], df.columns[6], df.columns[7], df.columns[8], df.columns[9],  "File Name"))
             with st.expander("Y-axis:"):
                 minimumy = st.slider("Select minimum value for the y-axis of the plot", -1.0, 1.0, -0.1)
                 maximumy = st.slider("Select maximum value for the y-axis of the plot", -1.0, 1.0, 0.1)
