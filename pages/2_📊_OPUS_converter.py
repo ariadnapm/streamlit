dd08624dc2aed7454d2b6ba596ea2cab51de0a0d
Ariadna <70442401+ariadnapm@users.noreply.github.com>
2024-03-26 15:33:26 +0100

    Update 2_📊_OPUS_converter.py

--- pages/2_📊_OPUS_converter.py
+++ pages/2_📊_OPUS_converter.py
@@ -128,7 +128,11 @@ with col2:
 
             
             min_y = np.min(signal)
-            data_min_signal.append(min_y)
+            if min_y < 0.0:
+                min_y = 0.0
+                data_min_signal.append(min_y)
+            else:
+                data_min_signal.append(min_y)
 
             max_y = np.max(signal)
             data_max_signal.append(max_y)
@@ -197,11 +201,7 @@ with col2:
 
 
             min_y = np.min(signal)
-            if min_y < 0.0:
-                min_y = 0.0
-                background_min_signal.append(min_y)
-            else:
-                background_min_signal.append(min_y)
+            background_min_signal.append(min_y)
 
             max_y = np.max(signal)
             background_max_signal.append(max_y)
