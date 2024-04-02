26c0991e81fbd067b5abfe28538efd50b0969879
Ariadna <70442401+ariadnapm@users.noreply.github.com>
2024-04-02 14:29:01 +0200

    Update and rename 5_Peaks_Finder.py to 5_🌍_Peaks_Finder.py

--- pages/5_🌍_Peaks_Finder.py
+++ pages/5_🌍_Peaks_Finder.py
@@ -9,10 +9,9 @@ import streamlit as st
 import os
 import statistics
 
-st.set_page_config(page_title="Chromatogram Graph Tool", page_icon="🌍", layout="wide")
+st.set_page_config(page_title="Peaks Finder", page_icon="🌍", layout="wide")
 
-st.markdown('# Chromatogram Graph Tool')
-st.sidebar.header("Chromatogram Graph Tool")
+st.markdown('# Peaks Finder')
 
 st.markdown(
     """
